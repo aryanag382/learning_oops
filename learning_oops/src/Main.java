99500a2ad82a33f8be31020d23af9e7250abba87
Aryan <aryanag382@gmail.com>
2026-02-08 21:43:06 +0530

    printed pattern

--- learning_oops/src/Main.java
+++ learning_oops/src/Main.java
@@ -1,9 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello");
-
+int n=4;
+int m=1;
+        for(int i=1;i<=5;i++){
+            for (int j=1;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=m;k++){
+                System.out.print("*");
+            }
+            n--;
+            m++;
+            System.out.println( );
+        }
 
     }
 }
